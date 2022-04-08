c5a53f0f4df60a14c9bd06a3bc5559d3ee6490e4
AhmadAsoev <asoev.ahmad@mail.ru>
2022-04-08 14:35:31 +0300

    addrout addInfo

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,24 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+)
+
+const (
+	POST = "POST"
 )
 
 func main() {
 	router := mux.NewRouter()
 	port := ":8080"
 
+	//AddInfo create new rout with POST method
+	router.HandleFunc("/info", handleFunc.AddInfo).Methods(POST)
+
+	fmt.Println("server is ready")
 	if err := http.ListenAndServe(port, router); err != nil {
 		log.Fatal("Server is not ready")
 	}
